821d0fe3de161b05604ef11d097ef47138f429ab
Jeff Lewis <jeffylew77@gmail.com>
2019-11-12 11:42:47 -0800

    [Dev] Adding skeleton for Hash Table

---
+++ linear-data-structures/hash-table.js
@@ -0,0 +1,29 @@
+/*
+Notes:
+- Time Complexity: O(1)
+- Space Complexity: 0(n)
+*/
+
+
+/*
+Tests:
+
+
+*/
+
+
+// Hash Table
+class HashTable {
+  constructor() {
+    this.storage = {};
+  }
+
+  //
+
+  //
+
+  //
+
+
+  //
+};
