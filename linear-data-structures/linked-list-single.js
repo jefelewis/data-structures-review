385a9a6c916383f9f5dff89b644755d8e2579308
Jeff Lewis <jeffylew77@gmail.com>
2019-11-12 16:09:12 -0800

    [Dev] Adding Linked List (Single)

---
+++ linear-data-structures/linked-list-single.js
@@ -0,0 +1,38 @@
+/*
+Notes:
+- Time Complexity: 
+- Space Complexity: 
+*/
+
+
+/*
+Tests:
+
+
+*/
+
+
+// Linked List (Single)
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  };
+
+  // Add Node
+
+
+  // Remove Node
+
+  
+  // Find Value
+};
+
+
+// Node
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  };
+};
