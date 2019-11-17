b19e77ef662a903629bc3ac38655a8b20ffc2637
Jeff Lewis <jeffylew77@gmail.com>
2019-11-17 11:31:48 -0800

    [Dev] Adding Tree skeleton

---
+++ non-linear-data-structures/tree.js
@@ -0,0 +1,31 @@
+/*
+Notes:
+- Non Linear
+- Time Complexity: 
+- Space Complexity: 
+*/
+
+
+/*
+Tests:
+
+
+*/
+
+
+// Tree
+class Tree {
+  constructor(value) {
+    this.value = value;
+    this.children = [];
+  };
+  
+  // DATA FUNCTIONS
+
+
+
+  // HELPER FUNCTIONS
+
+
+
+};
